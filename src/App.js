0877809ee49c47ec7eba4b2005293252de62268a
Brian <brianrondolo@yahoo.com>
2021-10-15 21:56:27 -0700

    replaced p tags with section tags

--- src/App.js
+++ src/App.js
@@ -10,28 +10,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Food Now!</h1>
-        <p className="App-question">
+        <section className="App-question">
           <h4>1. Choose categories!</h4>
-        </p>
-        <p className="App-location">
+        </section>
+        <section className="App-location">
           <h4>2. WYA?</h4>
-        </p>
-        <p className="App-distance">
+        </section>
+        <section className="App-distance">
           <h4>3. How lazy are you?</h4>
           <DistanceSlider />
-        </p>
-        <p className="App-price">
+        </section>
+        <section className="App-price">
           <h4>4. How cheap are you?</h4>
           <PriceSlider />
-        </p>
-        <p className="App-rating">
+        </section>
+        <section className="App-rating">
           <h4>5. How low is the bar?</h4>
           <Rating name="half-rating" defaultValue={0} precision={0.5} size="large" />
-        </p>
-        <p className="App-attributes">
+        </section>
+        <section className="App-attributes">
           <h4>6. Additional attributes</h4>
           <Attributes />
-        </p>
+        </section>
         <Button variant ="contained">Food Now!</Button>
       </header>
 
